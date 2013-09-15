1a14a03b787e8ec0eb484d4cbb22a181ae5e482b
Marie Wong <wlj.marie@gmail.com>
2013-09-15 15:51:36 +0800

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+	validates :name, presence: true, length: { minimum: 3 }
+
+	VALID_EMAIL_REGEX = /\A[\w+\-\.]+@[a-z\d\-\.]+\.[a-z]+\z/i
+
+	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+
+	has_secure_password
 end
